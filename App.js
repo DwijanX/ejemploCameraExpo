d087f003b099af299f012fb1bc2c86bae23fd896
dwijan <christianriveroarnez@gmail.com>
2024-03-01 17:08:27 -0400

    fullcommit

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { StyleSheet, View } from "react-native";
+import { WebView } from "react-native-webview";
+import { Camera, CameraType } from "expo-camera";
 
 export default function App() {
+  const [hasPermission, setHasPermission] = React.useState(null);
+
+  React.useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      console.log(status);
+      setHasPermission(status === "granted");
+    })();
+  }, []);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WebView
+      style={styles.container}
+      source={{
+        uri: "https://mgjb5hzl-5500.brs.devtunnels.ms/test.html",
+      }}
+      originWhitelist={["*"]}
+      javaScriptEnabled={true}
+      domStorageEnabled={true}
+      startInLoadingState={true}
+      allowFileAccess={true}
+      scalesPageToFit={true}
+      onNavigationStateChange={() => {}}
+      mediaPlaybackRequiresUserAction={false}
+      mediaCapturePermissionGrantType="grantIfSameHostElsePrompt"
+    />
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 50,
   },
 });
